89d896c38ce46007abf2152b2acd31b7d64053ec
Bernat Frangi <bernat.frangi@gmail.com>
2023-08-02 18:16:39 +0200

    Add specs for Task scopes

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'scopes' do
+    describe '.completed' do
+      it 'returns only completed tasks' do
+        completed_task = create(:task, completed: Time.now)
+        not_completed_task = create(:task, completed: nil)
+
+        expect(Task.completed).to eq([completed_task])
+      end
+    end
+
+    describe '.not_completed' do
+      it 'returns only not completed tasks' do
+        completed_task = create(:task, completed: Time.now)
+        not_completed_task = create(:task, completed: nil)
+
+        expect(Task.not_completed).to eq([not_completed_task])
+      end
+    end
+  end
 end
