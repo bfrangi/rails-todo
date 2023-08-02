f6a84cf3cab1755bb135c434d866c3fc77630a13
Bernat Frangi <bernat.frangi@gmail.com>
2023-08-02 18:15:43 +0200

    Add completed/not_completed scopes to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+  scope :completed, -> { where.not(completed: nil) }
+  scope :not_completed, -> { where(completed: nil) }
+
   validates :content, presence: true
   belongs_to :user
 end
