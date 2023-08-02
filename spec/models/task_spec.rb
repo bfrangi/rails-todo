23d7d9fe8813aa5ed94a30d49ca5e18e0d94f2b8
Bernat Frangi <bernat.frangi@gmail.com>
2023-08-02 18:21:49 +0200

    Add specs for `completed?` Task method

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe Task, type: :model do
       end
     end
   end
+
+  describe 'methods' do
+    describe '.completed?' do
+      it 'returns false when completed is set to nil' do
+        not_completed_task = build(:task)
+
+        expect(not_completed_task.completed?).to eq(false)
+      end
+
+      it 'returns false when completed is a date in the future' do
+        not_completed_task = build(:task, completed: 1.day.from_now)
+
+        expect(not_completed_task.completed?).to eq(false)
+      end
+
+      it 'returns true when completed is a date in the past' do
+        completed_task = build(:task, :completed)
+
+        expect(completed_task.completed?).to eq(true)
+      end
+    end
+  end
 end
