a0e71319daed6527786d3974acea07081e011b8d
Bernat Frangi <bernat.frangi@gmail.com>
2023-08-02 18:20:37 +0200

    Update Task scopes + add method to determine task completion

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,15 @@
 class Task < ApplicationRecord
-  scope :completed, -> { where.not(completed: nil) }
-  scope :not_completed, -> { where(completed: nil) }
+  scope :completed, lambda {
+    where('completed <= ?', Time.now)
+  }
+  scope :not_completed, lambda {
+    where('completed IS NULL OR completed > ?', Time.now)
+  }
 
   validates :content, presence: true
   belongs_to :user
+
+  def completed?
+    !completed.nil? && completed <= Time.now
+  end
 end
